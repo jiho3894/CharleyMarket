c846c9fe7743f5b1b2e72cf50132df0d325453b4
김지호 <crsn1111@gmail.com>
2022-07-31 20:36:46 +0900

    [feat] prisma correct DB

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,7 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Charley",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
   return res.status(200).end();
 };
 
