7a9c2bf5d92fae569f2793f10d56192a2d10a5af
김지호 <crsn1111@gmail.com>
2022-08-18 21:36:43 +0900

    [add] setting twilo

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
@@ -59,6 +63,9 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     }
     console.log(user);
   } */
-  return res.status(200).end();
+  // return res.status(200).end();
+  return res.json({
+    ok: true,
+  });
 }
 export default withHandler("POST", handler);
